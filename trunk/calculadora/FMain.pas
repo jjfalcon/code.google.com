bbdd1ce1116a725cfa445bc646a43fb0c15eaa0a
jjfalcon <jjfalcon@913e7608-4163-11de-bba7-a9cb5bbe15e6>
2009-10-06 17:17:01 +0000

    actualizar UI segun original

--- trunk/calculadora/FMain.pas
+++ trunk/calculadora/FMain.pas
@@ -4,13 +4,11 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, jpeg, ExtCtrls, UCalculator, Menus, ActnList;
+  Dialogs, StdCtrls, jpeg, ExtCtrls, UCalculator, Menus, ActnList, XPMan,
+  Buttons;
 
 type
   TFormMain = class(TForm)
-    Image1: TImage;
-    lblResult: TLabel;
-    img7: TImage;
     MainMenu1: TMainMenu;
     mnuEdicion: TMenuItem;
     mnuVer: TMenuItem;
@@ -32,21 +30,39 @@ type
     actHelp: TAction;
     btnHelp: TButton;
     btnAbout: TButton;
-    imgMC: TImage;
-    imgMR: TImage;
-    imgMS: TImage;
-    imgMA: TImage;
     imgCE: TImage;
-    img4: TImage;
     ImgBack: TImage;
-    img1: TImage;
-    img0: TImage;
-    imgSumar: TImage;
-    imgCalcular: TImage;
-    imgRestar: TImage;
-    imgSQRT: TImage;
-    imgInverso: TImage;
-    imgSigno: TImage;
+    XPManifest1: TXPManifest;
+    btn9: TSpeedButton;
+    SpeedButton2: TSpeedButton;
+    SpeedButton3: TSpeedButton;
+    SpeedButton4: TSpeedButton;
+    SpeedButton5: TSpeedButton;
+    SpeedButton6: TSpeedButton;
+    SpeedButton7: TSpeedButton;
+    SpeedButton8: TSpeedButton;
+    SpeedButton9: TSpeedButton;
+    SpeedButton10: TSpeedButton;
+    SpeedButton11: TSpeedButton;
+    SpeedButton12: TSpeedButton;
+    SpeedButton15: TSpeedButton;
+    SpeedButton13: TSpeedButton;
+    SpeedButton14: TSpeedButton;
+    SpeedButton16: TSpeedButton;
+    SpeedButton17: TSpeedButton;
+    SpeedButton18: TSpeedButton;
+    SpeedButton19: TSpeedButton;
+    SpeedButton20: TSpeedButton;
+    SpeedButton21: TSpeedButton;
+    SpeedButton22: TSpeedButton;
+    SpeedButton23: TSpeedButton;
+    SpeedButton24: TSpeedButton;
+    SpeedButton25: TSpeedButton;
+    SpeedButton26: TSpeedButton;
+    SpeedButton27: TSpeedButton;
+    Shape1: TShape;
+    lblResult: TLabel;
+    Panel1: TPanel;
     procedure img7Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormClose(Sender: TObject; var Action: TCloseAction);
